c406022e299f3f5fb5bfd7cfba53870e7a1fd5eb
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2023-12-05 14:42:04 +0000

    Protobuf codegen

--- data-loader/src/bin/build_proto.rs
+++ data-loader/src/bin/build_proto.rs
@@ -8,10 +8,11 @@ fn main() {
     // Create the full path for the .proto file
     let proto_folder = current_dir.join("proto");
     let proto_file = proto_folder.join("chess.proto");
-    let out_dir = current_dir.join("data-loader/src/proto");
+    let out_dir = current_dir.join("data-loader/src");
 
     prost_build::Config::new()
         .out_dir(out_dir)
+        .default_package_filename("proto")
         .compile_protos(&[proto_file], &[proto_folder])
         .unwrap();
 }
